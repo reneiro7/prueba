969f66c7258c63bb6dca1794da6bfb51a70119c7
Reneiro Muñoz <renecallejasjeva.2015@gmail.com>
2021-02-21 22:41:56 -0500

    -

---
+++ R/sum.R
@@ -0,0 +1,21 @@
+
+#' @title funcion suma
+#' @description  estimador de núcleo de la densidad.
+#' @param x vector de datos cuyo histograma se va a calcular
+#' @param y valor lógico: TRUE=Superponer estimador de núcleo de la densidad
+#' @return suma
+#' @export normalHist
+#' @examples
+#' sum(3,3)
+#' 6
+#' normalHist(u,dens=TRUE)
+#' normalHist(u,dens=TRUE,col="lightcyan")
+
+sumrey<- function(x,y){
+  x+y
+  }
+
+
+mulrey<- function(x,y){
+  x*y
+  }
